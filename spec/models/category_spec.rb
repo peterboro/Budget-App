740b51f2071176bd5bd757a9cfa4549a39aace97
Peter Njuguna <Ptahbn@gmail.com>
2023-01-05 13:51:47 +0300

    Fix linters

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Category, type: :model do
   before :each do
     @user = User.create(name: 'John', email: 'john@gmail.com', password: '123456')
     @category = Category.new(name: 'Food', icon: 'food.png', user_id: @user.id)
-  end   
+  end
 
   it 'name should be valid with correct parameters' do
     expect(@category).to be_valid
